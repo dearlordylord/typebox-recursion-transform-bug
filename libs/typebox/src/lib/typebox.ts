8bdd3109f9cd9482154e3524dc2740b4d71e56b4
Igor Loskutov <igor.loskutoff@gmail.com>
2024-07-22 18:47:32 -0400

    more test cases

--- libs/typebox/src/lib/typebox.ts
+++ libs/typebox/src/lib/typebox.ts
@@ -1,10 +1,11 @@
-import { Type, StaticDecode } from '@sinclair/typebox';
+import { Type, StaticDecode, Static } from '@sinclair/typebox';
 import { Value } from '@sinclair/typebox/value';
 
 const Recursion = Type.Recursive(Self => Type.Object({
+  id: Type.String(),
   children: Type.Transform(
     Type.Array(Self)
-  ).Decode((v: never[]/*inferred as never[]; I just set it here for explicit reporting*/) => v).Encode(v => v),
+  ).Decode((v/*inferred as never[];*/) => v).Encode(v => v),
 }));
 
 type Recursion = StaticDecode<typeof Recursion>;
@@ -15,3 +16,43 @@ const r: {children: never[]} = Value.Decode(Recursion, {
     { children: [{ children: [] }] },
   ],
 });
+
+function test(node: Recursion) {
+  const a: never[] = node.children[0];
+  // @ts-expect-error
+  const b = node.children[0].children[0].id;
+}
+
+const Recursion2 = Type.Recursive(Self => Type.Object({
+  id: Type.String(),
+  children: Type.Array(Self),
+}));
+
+type Recursion2 = StaticDecode<typeof Recursion2>;
+
+function test2(node: Recursion2) {
+  const a: string = node.children[0].children[0].id;
+}
+
+const Recursion3 = Type.Recursive(Self => Type.Object({
+  id: Type.String(),
+  child: Self,
+}));
+
+type Recursion3 = StaticDecode<typeof Recursion3>;
+
+function test3(node: Recursion3) {
+  const a = node.child.child.id;
+}
+
+const Recursion4 = Type.Recursive(This => Type.Object({    // const Node = {
+  id: Type.String(),
+  nodes: Type.Array(This)
+}), { $id: 'Node' })
+
+type Node = StaticDecode<typeof Recursion4>
+
+function test4(node: Node) {
+  const id = node.nodes[0].nodes[0].id
+}
+
